961d307f05915ef3f9b864a2566c2c013e271697
sofaprog <sofia_makoveeva@mail.ru>
2026-03-15 00:48:28 +0300

    add12

--- test/tests.cpp
+++ test/tests.cpp
@@ -130,18 +130,6 @@ TEST(PrecisionTest, HandlesHighPrecision) {
     EXPECT_NEAR(circle.getArea(), expectedArea, 1e-12);
 }
 
-TEST(CircleEdgeCasesTest, HandlesVeryLargeRadius) {
-    Circle circle(1e6);
-    EXPECT_NEAR(circle.getArea(), Circle::PI * 1e12, 1e-3);
-    EXPECT_NEAR(circle.getFerence(), 2 * Circle::PI * 1e6, 1e-3);
-}
-
-TEST(PrecisionTest, HandlesHighPrecision) {
-    Circle circle(0.1);
-    double expectedArea = Circle::PI * 0.01;
-    EXPECT_NEAR(circle.getArea(), expectedArea, 1e-12);
-}
-
 TEST(ConsistencyTest, SetAndGetMethodsWorkTogether) {
     Circle circle(5.0);
     circle.setRadius(7.0);
@@ -151,9 +139,4 @@ TEST(ConsistencyTest, SetAndGetMethodsWorkTogether) {
     EXPECT_NEAR(circle.getRadius(), 7.0, 1e-10);
 }
 
-TEST(FractionalValuesTest, HandlesNonIntegerRadius) {
-    Circle circle(3.14159);
-    EXPECT_NEAR(circle.getRadius(), 3.14159, 1e-10);
-    EXPECT_NEAR(circle.getArea(), Circle::PI * 3.14159 * 3.14159, 1e-8);
-}
 
